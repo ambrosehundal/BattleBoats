e5638745f0810f5012c890dccc0b68e7284a3364
Ambrose Hundal <amshunda@ucsc.edu>
2018-06-06 22:41:21 -0700

    Update AgentTest.c

--- Lab9/AgentTest.c
+++ Lab9/AgentTest.c
@@ -9,8 +9,9 @@
 #include <plib.h>
 
 // User libraries
-
-
+#include "Agent.h"
+#include "Protocol.h"
+#include "Field.h"
 // **** Set any macros or preprocessor directives here ****
 
 // **** Declare any data types here ****
@@ -20,22 +21,54 @@
 
 // **** Declare any function prototypes here ****
 
-
-
-int main()
-{
+int main() {
     BOARD_Init();
+    // Field myField;
+    // FieldInit(&myField, FIELD_POSITION_EMPTY);
+
+
+    /******************************************************************************
+     * Your code goes in between this comment and the following one with asterisks.
+     *****************************************************************************/
+    //Testing for AgentInit()
+    AgentInit();
+    
+    
+    
+
+    //Testing for AgentGetStatus
+    int a = 0, b = 0;
+    uint8_t agentlives = AgentGetStatus();
+    if (agentlives == 0) {
+        a++;
+        printf("Test passed!\n");
+    }
+
+    if (a == 1) {
+        printf("AgentGetStatus() tests PASSSED %d/1\n", a);
+    }
+    
+    
+    //Testing for AgentGetEnemyStatus()
+    
+    uint8_t enemylives = AgentGetEnemyStatus();
+    if (enemylives == 0) {
+        b++;
+        printf("Test passed!\n");
+    }
+
+    if (b == 1) {
+        printf("AgentEnemyGetStatus() tests PASSSED %d/1\n", b);
+    }
 
 
 
-/******************************************************************************
- * Your code goes in between this comment and the following one with asterisks.
- *****************************************************************************/
 
 
-/******************************************************************************
- * Your code goes in between this comment and the preceeding one with asterisks
- *****************************************************************************/
+
+    /******************************************************************************
+     * Your code goes in between this comment and the preceeding one with asterisks
+     *****************************************************************************/
 
     while (1);
 }
